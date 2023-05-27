87555996016b709dc97c123a2fe28c2436c8b271
Wellington Vieira <tomvieira@outlook.com>
2023-05-26 22:18:52 -0300

    admin personalizado

--- siscostura/website/admin.py
+++ siscostura/website/admin.py
@@ -1,6 +1,13 @@
 from django.contrib import admin
 from siscostura.models import Pedido
+from siscostura.models import StatusPedido
+from siscostura.models import Cliente
+from siscostura.models import ItemPedido
 
 # Register your models here.
 
 admin.site.register(Pedido)
+admin.site.register(StatusPedido)
+admin.site.register(Cliente)
+admin.site.register(ItemPedido)
+admin.site.site_header = "Administração SisCostura"
